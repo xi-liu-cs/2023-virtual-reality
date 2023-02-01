613f36554ce91a6b4d3c19a050a1d666d0e082f3
Xi Liu <xi1331@126.com>
2023-02-01 17:09:20 -0500

    Update scenes.js

--- js/scenes/scenes.js
+++ js/scenes/scenes.js
@@ -8,10 +8,8 @@ export default () => {
 
    return {
       enableSceneReloading: true,
-      scenes: [ 
-         { name: "DemoExample" , path: "./demoExample.js" },
-         { name: "DemoKP0"     , path: "./demoKP0.js"     },
-         { name: "DemoKP1"     , path: "./demoKP1.js"     },
+      scenes: [
+         { name: "xi_liu", path: "./xi_liu.js" }
       ]
    };
 }
