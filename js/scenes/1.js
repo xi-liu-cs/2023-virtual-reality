cbc2b3be10c6af2e54ff8d37a68b0cb15501b894
Xi Liu <xi1331@126.com>
2023-02-05 17:01:46 -0500

    update

--- js/scenes/1.js
+++ js/scenes/1.js
@@ -3,16 +3,23 @@ import { controllerMatrix, buttonState, joyStickState } from "../render/core/con
 
 let leftTriggerPrev = false;
 let rightTriggerPrev = false;
+let right1pre = false;
+let right3pre = false;
 
 let MP = cg.mTranslate(0,1,.5);
+let MP2 = cg.mTranslate(0,-1,.5);
 let A = [0,0,0];
 let MA = cg.mIdentity();
+let MA1 = cg.mIdentity();
+let MA2 = cg.mIdentity();
+let tex = '../media/textures/planet.jpg';
 
 export const init = async model => {
 
    // CREATE THE BOX.
 
-   let box = model.add('sphere');
+   let box = model.add('tubeY').texture(tex);
+   let obj;
 
    // FUNCTION TO RETURN TRUE IF A POINT IS INSIDE THE BOX, OTHERWISE FALSE.
 
@@ -24,11 +31,16 @@ export const init = async model => {
 
       // THEN WE JUST NEED TO SEE IF THE RESULT IS INSIDE A UNIT CUBE.
 
-      return q[0] >= -1 & q[0] <= 1 &&
+      /* return q[0] >= -1 & q[0] <= 1 &&
              q[1] >= -1 & q[1] <= 1 &&
-             q[2] >= -1 & q[2] <= 1 ;
+             q[2] >= -1 & q[2] <= 1 ; */
+      
+      return q[0] >= -2 & q[0] <= 2 &&
+      q[1] >= -2 & q[1] <= 2 &&
+      q[2] >= -2 & q[2] <= 2 ;
    }
 
+   let pre_time = model.time;
    model.animate(() => {
 
       // FETCH THE MATRIXES FOR THE LEFT AND RIGHT CONTROLLER.
@@ -85,6 +97,18 @@ export const init = async model => {
 	 // IF THE RIGHT TRIGGGER IS SQUEEZED
 
          let rightTrigger = buttonState.right[0].pressed;
+         /* let right1 = buttonState.right[1].pressed,
+         right2 = buttonState.right[2].pressed,
+         right3 = buttonState.right[3].pressed,
+         right4 = buttonState.right[4].pressed,
+         right5 = buttonState.right[5].pressed,
+         right6 = buttonState.right[6].pressed;
+         if(right1) box.color(1,0,0); // lower left elliptical button
+         if(right2) box.color(0,1,0);
+         if(right3) box.color(0,0,0); // thumbstick
+         if(right4) box.color(1,1,0); // a
+         if(right5) box.color(0,1,1); // b
+         if(right6) box.color(1,0,1); */
 	 if (rightTrigger) {
 
 	    // COLOR THE BOX BLUE AND MOVE AND ROTATE THE BOX.
@@ -99,58 +123,49 @@ export const init = async model => {
 	    MA = MB;                       // REMEMBER PREVIOUS MATRIX.
          }
          rightTriggerPrev = rightTrigger;
+   if(buttonState.right[1].pressed)
+   {
+      box.color(1, 0, 0);
+      model.add('donut').texture(tex);
+      obj = model.add('cube').texture(tex);
+      let MB1 = mr.slice();
+      if (!right1pre)                // ON RIGHT DOWN EVENT:
+      {
+         box.add('tubeX').texture(tex);
+         MA1 = MB1;                  // INITIALIZE PREVIOUS MATRIX.
+      }                    
+      else
+         MP2 = cg.mMultiply(cg.mMultiply(MB1, cg.mInverse(MA1)), MP2);
+      MA1 = MB1;      
+   }
+   if(buttonState.right[3].pressed) // thumbstick
+   {
+      box.color(0, 0, 0);
+      let MB2 = mr.slice();
+      if (!right3pre)                // ON RIGHT DOWN EVENT:
+      {
+         box.add('tubeZ').texture(tex);
+         MA2 = MB2;                  // INITIALIZE PREVIOUS MATRIX.
+      }                    
+      else
+         MP = cg.mMultiply(cg.mMultiply(MB2, cg.mInverse(MA2)), MP);
+      MA2 = MB2;        
+   }
+   right3pre = buttonState.right[3].pressed;
+   if(buttonState.right[4].pressed) // a
+   {
+      box.color(1, 1, 0);
+      MP = cg.mMultiply(MP, cg.mScale(1.1, 1.1, 1.1));
+   }
+   if(buttonState.right[5].pressed) // b
+   {
+      box.color(0, 1, 1);
+      MP = cg.mMultiply(MP, cg.mScale(.9, .9, .9));
+   }
       }
 
       // DISPLAY THE BOX.
-   
-      box.setMatrix(MP).scale(.1);
+      box.setMatrix(MP).scale(.2); 
+      obj.setMatrix(MP2).scale(.2);
    });
 }
-
-// import 'https://cdn.jsdelivr.net/npm/@webxr-polyfill/polyfill@0.2.2/dist/polyfill.min.js';
-
-// navigator.xr.requestDevice().then(device => {
-//     device.supportsSession({ mode: 'immersive-vr' }).then(function (supported) {
-//       if (supported) {
-//         // Start an XR session
-//         device.requestSession({ mode: 'immersive-vr' }).then(session => {
-//           let xrSession = session;
-//           // Get reference to XR controller
-//           let controller = xrSession.inputSources[0].gamepad;
-  
-//           // Check for button events on the XR controller
-//           controller.addEventListener("buttonchange", (event) => {
-//             // Check if the button is being pressed
-//             if (event.pressed) {
-//               // Check which button is being pressed
-//               switch (event.button) {
-//                 case 0: // Button 0, move XR origin along the x-axis
-//                   xrSession.updateWorldTrackingState({
-//                     transform: new XRRigidTransform({
-//                       position: new DOMPoint(xrSession.worldInformation.identity.position.x + 0.1, 0, 0)
-//                     })
-//                   });
-//                   break;
-//                 case 1: // Button 1, move XR origin along the y-axis
-//                   xrSession.updateWorldTrackingState({
-//                     transform: new XRRigidTransform({
-//                       position: new DOMPoint(0, xrSession.worldInformation.identity.position.y + 0.1, 0)
-//                     })
-//                   });
-//                   break;
-//                 case 2: // Button 2, move XR origin along the z-axis
-//                   xrSession.updateWorldTrackingState({
-//                     transform: new XRRigidTransform({
-//                       position: new DOMPoint(0, 0, xrSession.worldInformation.identity.position.z + 0.1)
-//                     })
-//                   });
-//                   break;
-//                 default:
-//                   break;
-//               }
-//             }
-//           });
-//         });
-//       }
-//     });
-//   });
--- js/scenes/scenes.js
+++ js/scenes/scenes.js
@@ -3,15 +3,18 @@ import { Gltf2Node } from "../render/nodes/gltf2.js";
 
 export default () => {
    global.scene().addNode(new Gltf2Node({
-      url: "../media/gltf/0/1.glb"
-      // url: "../media/gltf/60_fifth_ave/60_fifth_ave.gltf"
+      url: "../media/gltf/cave/cave.gltf"
    })).name = "backGround";
+
+   global.scene().addNode(new Gltf2Node({
+      url: "../media/gltf/0/human.glb"
+   })).name = "human";
 
    return {
       enableSceneReloading: true,
       scenes: [ 
          {name: "xi_liu" , path: "./1.js"},
-         {name: '0', path: './0.js'}
+         {name: "0", path: "./0.js"}
       ]
    };
 }
