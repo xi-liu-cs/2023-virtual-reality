d7027ead4e4dc714686d3ce75465ca6a38c88c8b
Xi Liu <xi1331@126.com>
2023-02-26 10:14:12 -0500

    Update scenes.js

--- js/scenes/scenes.js
+++ js/scenes/scenes.js
@@ -8,16 +8,7 @@ export default () => {
 
    return {
       enableSceneReloading: true,
-      scenes: [ 
-         { name: "DemoExample"       , path: "./demoExample.js"       },
-         { name: "DemoKP0"           , path: "./demoKP0.js"           },
-         { name: "DemoKP1"           , path: "./demoKP1.js"           },
-         { name: "DemoKP2"           , path: "./demoKP2.js"           },
-         { name: "DemoKP3"           , path: "./demoKP3.js"           },
-         { name: "DemoCanvas"        , path: "./demoCanvas.js"        },
-         { name: "DemoTwoCubes"      , path: "./demoTwoCubes.js"      },
-         { name: "DemoTrianglesMesh" , path: "./demoTrianglesMesh.js" },
-         { name: "DemoOpacity"       , path: "./demoOpacity.js"       },
+      scenes: [
          { name: "DemoExample"       , path: "./demoExample.js"       },
          { name: "DemoKP0"           , path: "./demoKP0.js"           },
          { name: "DemoKP1"           , path: "./demoKP1.js"           },
