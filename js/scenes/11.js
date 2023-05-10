64608b1180d03ec9dfaca0fbfcb7e07d0b2bd8ba
Xi Liu <xi1331@126.com>
2023-05-10 16:42:34 -0400

    Update 11.js

--- js/scenes/11.js
+++ js/scenes/11.js
@@ -102,12 +102,12 @@ export const init = async model => {
             let src = '../../media/textures/image' + src_array[i] + '.png';
             let loadTexture = () => {
                 try {
-                gl.activeTexture (gl.TEXTURE0 + 2 + i);
-                gl.bindTexture   (gl.TEXTURE_2D, texture);
-                gl.pixelStorei   (gl.UNPACK_FLIP_Y_WEBGL, true);
-                gl.texImage2D    (gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-                gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-                gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+                gl.activeTexture(gl.TEXTURE0 + 2 + i);
+                gl.bindTexture(gl.TEXTURE_2D, texture);
+                gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
                 gl.generateMipmap(gl.TEXTURE_2D);
                 }
                 catch(e) { console.log(e); }
